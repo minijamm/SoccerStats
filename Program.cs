840e794547c701b1b8715d230610dce5e4bfdb16
James Millichip <james@millichip.me>
2016-07-22 23:11:09 +0100

    REad in CSV

--- Program.cs
+++ Program.cs
@@ -15,24 +15,25 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-            var fileName = Path.Combine(directory.FullName, "data.txt");
-            var file = new FileInfo(fileName);
-            if (file.Exists)
-            {
-                using (var reader = new StreamReader(file.FullName))
-                {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-
-                }
-
-            }
-          
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContent = ReadFile(fileName);
+            Console.WriteLine(fileContent);
+            Console.ReadLine();
 
 
         }
-          
+        
+        public static string ReadFile(string filename)
+        {
+
+            using (var reader = new StreamReader(filename))
+            {
+                return reader.ReadToEnd();
+            }
+        }  
+       
 
     }
+    
     
 }
